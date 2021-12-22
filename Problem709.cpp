447e93d52d3802696772fc9c4772ca25296aba07
satymsb <68340284+satymsb@users.noreply.github.com>
2021-12-22 21:02:11 +0530

    Create Problem709.cpp

---
+++ Problem709.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]<=90 && s[i]>=65)
+            {
+                s[i]+=32;
+            }
+        }
+        return s;
+    }
+};
