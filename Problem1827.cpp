e4987ad909ec6ee382954a2d879f82856916538a
satymsb <68340284+satymsb@users.noreply.github.com>
2021-12-25 13:23:24 +0530

    Create Problem1827.cpp

---
+++ Problem1827.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int count = 0;
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                continue;
+            }
+            else
+            {
+                count+=(nums[i]+1-nums[i+1]);
+                nums[i+1]=nums[i]+1;
+            }
+            
+        }
+        return count;
+    }
+};
